5333625c5090604510c47e26f71d7d2cbaf13945
zawadTazwar <113521009+zawadTazwar@users.noreply.github.com>
2025-03-26 06:17:18 -0230

    Update login.dart

--- lib/Views/login.dart
+++ lib/Views/login.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart' as auth_ui;
 import 'register.dart';
+import './fixer_register.dart'; // <-- Add this line
 import './AuthGate.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -10,25 +11,44 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return auth_ui.SignInScreen(
       providers: [auth_ui.EmailAuthProvider()],
-      showAuthActionSwitch: false, 
+      showAuthActionSwitch: false,
       actions: [
         auth_ui.ForgotPasswordAction((context, email) {
-          Navigator.push(context, MaterialPageRoute(builder: (_) => const auth_ui.ForgotPasswordScreen()));
+          Navigator.push(context,
+              MaterialPageRoute(builder: (_) => const auth_ui.ForgotPasswordScreen()));
         }),
         auth_ui.AuthStateChangeAction((context, state) {
           if (state is auth_ui.SignedIn || state is auth_ui.UserCreated) {
-            Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => const AuthGate()));
+            Navigator.pushReplacement(
+                context, MaterialPageRoute(builder: (_) => const AuthGate()));
           }
         }),
       ],
       footerBuilder: (context, _) {
-        return TextButton(
-          child: const Text("Don't have an account? Register here"),
-          onPressed: () {
-            Navigator.push(context, MaterialPageRoute(builder: (_) => const CustomRegisterScreen()));
-          },
+        return Column(
+          children: [
+            TextButton(
+              child: const Text("Don't have an account? Register as Problemer"),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => const CustomRegisterScreen()),
+                );
+              },
+            ),
+            TextButton(
+              child: const Text("Register as Fixer"),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => const FixerRegisterScreen()),
+                );
+              },
+            ),
+          ],
         );
       },
     );
   }
 }
+
